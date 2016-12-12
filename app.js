319e6264459cd65357ea6abcf9408131933933f4
Irfan Rasool <irfan@web-flight.nl>
2016-12-12 16:17:40 +0530

    CONST example

--- app.js
+++ app.js
@@ -15,3 +15,25 @@ console.log(age); // o/p => Uncaught ReferenceError: age is not defined */
  console.log('age: ', age); // o/p => age:  40 */
 
 
+/*
+* CONST examples :
+* */
+
+/* const AGE = 30;
+AGE = 40; // o/p => error because u can't change the values
+*/
+
+
+/*const AGES = [10, 20, 30, 40];
+AGES.push(50);
+console.log(AGES);
+// o/p => this won't give u error it will print all the values. Because we are not changing the reference only the value. That is why we are able to change
+*/
+
+// const OBJ1 = {
+//     age: 40
+// }
+// OBJ1.age = 30;
+// console.log(OBJ1); // o/p Object {age: 30} again same thing. We are not changing reference only the value.
+
+
