0f674b48cc4f21e571a19ebe52be8ec1a72a60c3
Irfan Rasool <irfan@web-flight.nl>
2016-12-12 16:08:06 +0530

    let example

---
+++ app.js
@@ -0,0 +1,17 @@
+/*
+* let and const examples:
+* */
+
+// let has a block scope advantage which var was missing. chk example
+/* if (true) {
+    let age = 30;
+}
+console.log(age); // o/p => Uncaught ReferenceError: age is not defined */
+
+/*let age = 40;
+ if (true) {
+ let age = 30;
+ }
+ console.log('age: ', age); // o/p => age:  40 */
+
+
